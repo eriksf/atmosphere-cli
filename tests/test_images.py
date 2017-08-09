62c6f8e84d162488c6e2d2a900b7d97abb0c53cc
Erik Ferlanti <eferlanti@tacc.utexas.edu>
2017-08-09 16:17:59 -0500

    updated tests

--- tests/test_images.py
+++ tests/test_images.py
@@ -11,21 +11,21 @@ class TestImages(object):
         start_mock_server(cls.mock_server_port)
 
     def test_getting_images_when_response_is_not_ok(self):
-        api = AtmosphereAPI('username', 'password', base_url=self.mock_users_bad_base_url)
+        api = AtmosphereAPI('token', base_url=self.mock_users_bad_base_url)
         response = api.get_images()
         assert not response
 
     def test_getting_images_when_response_is_ok(self):
-        api = AtmosphereAPI('username', 'password', base_url=self.mock_users_base_url)
+        api = AtmosphereAPI('token', base_url=self.mock_users_base_url)
         response = api.get_images()
         assert response['results'][0]['name'] == 'name'
 
     def test_getting_image_when_response_is_not_ok(self):
-        api = AtmosphereAPI('username', 'password', base_url=self.mock_users_bad_base_url)
+        api = AtmosphereAPI('token', base_url=self.mock_users_bad_base_url)
         response = api.get_image(1)
         assert not response
 
     def test_getting_image_when_response_is_ok(self):
-        api = AtmosphereAPI('username', 'password', base_url=self.mock_users_base_url)
+        api = AtmosphereAPI('token', base_url=self.mock_users_base_url)
         response = api.get_image(1)
         assert response['id'] == 1 and response['name'] == 'name'
